8b78c21b1d431516ade172441ff3a222c70df1eb
lindsfitz <lindsaytfitz@gmail.com>
2021-10-03 11:56:52 -0700

    adding icons into js code for current and forecast elements

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # weather-dashboard
 06 Server Side APIs: Weather Dashboard
 
-Your Task
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
 
-Use the OpenWeather One Call API to retrieve weather data for cities. Read through the documentation for setup and usage instructions. You will use localStorage to store any persistent data. For more information on how to work with the OpenWeather API, refer to the Full-Stack Blog on how to use API keys.
+This application uses the OpenWeather API to retrieve weather data for cities. 
+
+This data is used to display a weather dashboard for multiple cities that is dynamically updated. 
+The past searches are saved and can be reused as the search input by clicking on them.
+
 
 User Story
 AS A traveler
@@ -22,56 +24,14 @@ WHEN I view future weather conditions for that city
 THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
-Mock-Up
+
+
+
+# Mock-Up
 The following image shows the web application's appearance and functionality:
 
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
+<img src=./assets/images/laptop-mockup.jpg>
 
-The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.
-
-Grading Requirements
-This homework is graded based on the following criteria:
-
-Technical Acceptance Criteria: 40%
-Satisfies all of the above acceptance criteria plus the following:
-
-Uses the OpenWeather API to retrieve weather data.
-
-Uses localStorage to store persistent data.
-
-Deployment: 32%
-Application deployed at live URL.
-
-Application loads with no errors.
-
-Application GitHub URL submitted.
-
-GitHub repository that contains application code.
-
-Application Quality: 15%
-Application user experience is intuitive and easy to navigate.
-
-Application user interface style is clean and polished.
-
-Application resembles the mock-up functionality provided in the homework instructions.
-
-Repository Quality: 13%
-Repository has a unique name.
-
-Repository follows best practices for file structure and naming conventions.
-
-Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-Repository contains multiple descriptive commit messages.
-
-Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-Review
-You are required to submit BOTH of the following for review:
-
-The URL of the functional, deployed application.
-
-The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
 
 
 # Deployed Link
--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,9 @@
-// var apiKey = adf080c4900ab48938f6770e1ae7a9c0;
-//One call:
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=adf080c4900ab48938f6770e1ae7a9c0
-
 
 //current weather for main weather card; one call for future forecast cards
 var today = moment().format("MMMM Do, YYYY");
 var searchHistory = JSON.parse(localStorage.getItem("search-history")) || [];
 
+//call function to pull past searches from localstorage and display them along left side of page
 init();
 
 $("#search-button").on("click", function(){
@@ -17,6 +14,7 @@ $("#search-button").on("click", function(){
     saveSearch(searchInput);
 })
 
+//adds listener to entire div and if a button from a past search is clicked, the text within becomes the new search input
 $("#history").on("click", function(event){
     // element = event.target
     newInput = $(event.target).text()
@@ -24,21 +22,23 @@ $("#history").on("click", function(event){
     
 })
 
+//function to pull the data from the weather API and create elements to display the information 
 function weatherSearch(input) {
     $("#current").empty();
-    $('#fiveday').empty();
     var APIurl = "https://api.openweathermap.org/data/2.5/weather?q="+input+"&appid=adf080c4900ab48938f6770e1ae7a9c0&units=imperial"
     fetch(APIurl)
     .then(response => {return response.json()})
     .then(data => {
         console.log(data)
-        var cityname,temp,wind,humidity;
+        var cityname,temp,wind,humidity,icon;
         cityname = $("<h2>").addClass("city-name").text(data.name+"  -  "+today);
+        var iconLink = "http://openweathermap.org/img/wn/"+data.weather[0].icon+"@2x.png";
+        icon = $('<img>').attr('src',iconLink).attr("style","width:40px;height:40px;")
         temp = $("<p>").text("Temp: "+data.main.temp+"℉");
         wind = $("<p>").text("Wind: "+data.wind.speed+"MPH");
         humidity = $("<p>").text("Humidity: "+data.main.humidity+"%");
 
-        $("#current").append(cityname,temp,wind,humidity);    
+        $("#current").append(cityname,icon,temp,wind,humidity);    
         
         var lat = data.coord.lat;
         var lon = data.coord.lon;
@@ -47,8 +47,10 @@ function weatherSearch(input) {
     })
 }
 
+//pulling the data from the weather API to display the 5 day forcast. Includes an if statement to color code the UV index and a for loop to create the elements for each of the 5 days and append to the page. 
 function forecastSearch(lat,lon) {
     $("#fiveday").empty();
+    $('#fivedayHeader').empty();
     var APIurl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&appid=adf080c4900ab48938f6770e1ae7a9c0&units=imperial"
     fetch(APIurl)
     .then(response => {return response.json()})
@@ -76,19 +78,24 @@ function forecastSearch(lat,lon) {
             var temp = $("<p>").addClass("card-text").text("Temp: "+data.daily[i].temp.day+"℉");
             var wind = $("<p>").addClass("card-text").text("Wind: "+data.daily[i].wind_speed+"MPH");
             var humidity = $("<p>").addClass("card-text").text("Humidity: "+data.daily[i].humidity+"%");
+            var iconLink = "http://openweathermap.org/img/wn/"+data.daily[i].weather[0].icon+"@2x.png";
+            var icon = $('<img>').attr('src',iconLink).attr("style","width:30px;height:30px;")
+            
 
             var cardBody = $("<div>").addClass("card-body");
             var cardDiv = $("<div>").addClass("card mt5").attr("style","width:11rem;");
             var date = moment().add(i,"days").format("MMM Do, YYYY");
             var cardDate = $("<h5>").addClass("card-title").text(date);
 
-            $(cardBody).append(cardDate,temp,wind,humidity);
+            $(cardBody).append(cardDate,icon,temp,wind,humidity);
             $(cardDiv).append(cardBody);
             $("#fiveday").append(cardDiv);
         }
     })
 }
 
+
+//function to save the past searches as buttons on the left side and save them in local storage so they can be displayed on page refresh. if statement to make sure there are no repeat searches listed 
 function saveSearch(searchInput) {
    if (!searchHistory.includes(searchInput)) {
     var searchButton = $("<button>").addClass("list-group-item list-group-item-action").attr("style","width:15rem").text(searchInput);
@@ -98,6 +105,8 @@ function saveSearch(searchInput) {
     localStorage.setItem("search-history",JSON.stringify(searchHistory));
 }
 
+
+//on page load, pull history from local storage and create buttons
 function init() {
     var savedHistory = JSON.parse(localStorage.getItem("search-history")) || [];
     if (savedHistory === null) {
